6e37288e28ea0276a51d2cce6491f2520aa69d6f
Kang Yaw Ong <kangyaw.ong@infinite-code.com>
2015-11-27 15:32:13 +0800

    update

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# SUPER NOTE:
+
+- rollback the android google play service, to use commit `71dde2` version
+- ios is using latest master branch ios
+
+
 # Google+ Cordova/PhoneGap Plugin
 by [Eddy Verbruggen](http://twitter.com/eddyverbruggen)
 
@@ -5,13 +11,19 @@ by [Eddy Verbruggen](http://twitter.com/eddyverbruggen)
 
 1. [Description](#1-description)
 2. [Screenshots](#2-screenshots)
-3. [Google+ API setup](#3-google-api-setup)
-4. [Installation (CLI / Plugman)](#4-installation-phonegap-cli--cordova-cli)
-5. [Installation (PhoneGap Build)](#5-installation-phonegap-build)
-6. [Usage](#6-usage)
-7. [Troubleshooting](#7-troubleshooting)
-8. [Changelog](#8-changelog)
-9. [License](#9-license)
+3. [Installation (CLI / Plugman)](#3-installation-phonegap-cli--cordova-cli)
+4. [Google+ API setup](#4-google-api-setup)
+5. [Usage](#5-usage)
+6. [Troubleshooting](#6-troubleshooting)
+7. [Changelog](#7-changelog)
+8. [License](#8-license)
+
+
+## << --- Cordova Registry Warning [iOS]
+
+****Installing this plugin directly from Cordova Registry results in Xcode using a broken `GoogleOpenSource.framework` and `GooglePlus.framework`, this is because the current publish procedure to NPM breaks symlinks [CB-6092](https://issues.apache.org/jira/browse/CB-6092). Please install the plugin through a locally cloned copy or re-add the frameworks to Xcode after installation.****
+
+## ------------------------------------------ >>
 
 ## 1. Description
 
@@ -27,50 +39,39 @@ Android
 <img src="https://raw.githubusercontent.com/EddyVerbruggen/cordova-plugin-googleplus/master/screenshots/Android3.png" width="235" height="400"/>
 
  iOS
-
+ 
 <img src="https://raw.githubusercontent.com/EddyVerbruggen/cordova-plugin-googleplus/master/screenshots/iOS1.png" width="235" height="417"/>&nbsp;
 <img src="https://raw.githubusercontent.com/EddyVerbruggen/cordova-plugin-googleplus/master/screenshots/iOS2.png" width="235" height="417"/>&nbsp;
 <img src="https://raw.githubusercontent.com/EddyVerbruggen/cordova-plugin-googleplus/master/screenshots/iOS3.png" width="235" height="417"/>&nbsp;
-
-## 3. Google+ API setup
-To communicate with Google+ you need to do some tedious setup, sorry.
-
-### iOS
-To get your iOS API key, follow Step 1 of [this guide](https://developers.google.com/+/mobile/ios/getting-started)
-[get a configuration file here](https://developers.google.com/mobile/add?platform=ios&cntapi=signin).
-This `GoogleService-Info.plist` file contains the `REVERSED_CLIENT_ID` you'll need during installation.
-
-### Android
-To configure Android, follow Step 2 (Get a configuration file) of [this guide](https://developers.google.com/identity/sign-in/android/start). Once Google Sign-In is enabled Google will automatically create necessary credentials in Developer Console. There is no need to add the generated google-services.json file into your cordova project.
-
-Make sure you execute the `keytool` steps as well or authentication will fail.
-
-## 4. Installation (PhoneGap CLI / Cordova CLI)
+ 
+## 3. Installation (PhoneGap CLI / Cordova CLI)
 This plugin is compatible with [Cordova Plugman](https://github.com/apache/cordova-plugman), compatible with [PhoneGap 3.0 CLI](http://docs.phonegap.com/en/3.0.0/guide_cli_index.md.html#The%20Command-line%20Interface_add_features), here's how it works with the CLI (backup your project first!):
 
-Using the Cordova CLI and [npm](https://www.npmjs.com/package/cordova-plugin-googleplus)
+Using the Cordova CLI and the [Cordova Plugin Registry](http://plugins.cordova.io)
 ```
-$ cordova plugin add cordova-plugin-googleplus --variable REVERSED_CLIENT_ID=myreversedclientid
+$ cordova plugin add nl.x-services.plugins.googleplus
 $ cordova prepare
 ```
 
-To fetch the latest version from GitHub, use
+Or using the phonegap CLI
 ```
-$ cordova plugin add https://github.com/EddyVerbruggen/cordova-plugin-googleplus --variable REVERSED_CLIENT_ID=myreversedclientid
-$ cordova prepare
+$ phonegap local plugin add nl.x-services.plugins.googleplus
 ```
 
 GooglePlus.js is brought in automatically. There is no need to change or add anything in your html.
 
-## 5. Installation (PhoneGap Build)
-Add this to your config.xml:
-```xml
-<gap:plugin name="cordova-plugin-googleplus" source="npm">
-  <param name="REVERSED_CLIENT_ID" value="myreversedclientid" />
-</gap:plugin>
-```
+## 4. Google+ API setup
+To communicate with Google+ you need to do some tedious setup, sorry.
 
-## 6. Usage
+### iOS
+To get your iOS API key, follow Step 1 of [this guide](https://developers.google.com/+/quickstart/ios)
+
+### Android
+To configure Android, follow Step 1 of [this guide](https://developers.google.com/+/quickstart/android)
+
+Make sure you execute the `keytool` steps as well or authentication will fail.
+
+## 5. Usage
 Check the [demo app](demo) to get you going quickly, or hurt yourself and follow these steps.
 
 Note that none of these methods should be called before [`deviceready`](http://docs.phonegap.com/en/edge/cordova_events_events.md.html#deviceready) has fired.
@@ -97,9 +98,7 @@ window.plugins.googleplus.isAvailable(
 ```javascript
 window.plugins.googleplus.login(
     {
-      'scopes': '... ', // optional space-separated list of scopes, the default is sufficient for login and basic profile info
-      'offline': true, // optional and required for Android only - if set to true the plugin will also return the OAuth access token, that can be used to sign in to some third party services that don't accept a Cross-client identity token (ex. Firebase)
-      'webApiKey': 'api of web app', // optional API key of your Web application from Credentials settings of your project - if you set it the returned idToken will allow sign in to services like Azure Mobile Services
+      'iOSApiKey': '1234567890-abcdefghijklm74bfw.apps.googleusercontent.com'
       // there is no API key for Android; you app is wired to the Google+ API by listing your package name in the google dev console and signing your apk (which you have done in chapter 4)
     },
     function (obj) {
@@ -118,18 +117,16 @@ The success callback (second argument) gets a JSON object with the following con
  obj.email        // 'eddyverbruggen@gmail.com'
  obj.userId       // user id
  obj.displayName  // 'Eddy Verbruggen'
- obj.imageUrl     // 'http://link-to-my-profilepic.google.com'
- obj.idToken
- obj.oauthToken
-
- // these are only available on Android at the moment
  obj.gender       // 'male' (other options are 'female' and 'unknown'
+ obj.imageUrl     // 'http://link-to-my-profilepic.google.com'
  obj.givenName    // 'Eddy'
  obj.middleName   // null (or undefined, depending on the platform)
  obj.familyName   // 'Verbruggen'
  obj.birthday     // '1977-04-22'
  obj.ageRangeMin  // 21 (or null or undefined or a different number)
  obj.ageRangeMax  // null (or undefined or a number)
+ obj.idToken
+ obj.oauthToken
 ```
 
 ### Try silent login
@@ -143,8 +140,7 @@ The code is exactly the same a `login`, except for the function name.
 ```javascript
 window.plugins.googleplus.trySilentLogin(
     {
-      'offline': true, // optional and required for Android only - if set to true the plugin will also return the OAuth access token, that can be used to sign in to some third party services that don't accept a Cross-client identity token (ex. Firebase)
-      'webApiKey': 'api of web app' // optional API key of your Web application from Credentials settings of your project - if you set it the returned idToken will allow sign in to services like Azure Mobile Services 
+      'iOSApiKey': '1234567890-abcdefghijklm74bfw.apps.googleusercontent.com'
     },
     function (obj) {
       alert(JSON.stringify(obj)); // do something useful instead of alerting
@@ -177,25 +173,18 @@ window.plugins.googleplus.disconnect(
 );
 ```
 
-## 7. Troubleshooting
+## 6. Troubleshooting
 - Q: After authentication I'm not redirected back to my app.
 - A: You probably changed the bundle id of your app after installing this plugin. Make sure that (on iOS) the `CFBundleURLTypes` bit in your `.plist` file is the same as the actual bundle id originating from `config.xml`.
 
 - Q: I can't get authentication to work on Android. And why is there no ANDROID API KEY?
 - A: On Android you need to execute the `keytool` steps, see the installation instructions for details.
 
-## 8. Changelog
-4.0.6: Updated iOS GoogleSignIn SDK to 2.4.0. Thx #153!
-4.0.5: Fixed a broken import on iOS
-4.0.4: Using framework tags again for Android.
-4.0.3: On iOS `isAvailable` always returns try since that should be fine with the new Google SignIn framework. Re-added imageUrl to the result of SignIn on iOS.
-4.0.1: Login on Android would crash the app if `isAvailable` was invoked beforehand.
-4.0.0: Removed the need for `iosApiKey`, reverted Android to Google playservices framework for wider compatibility, documented scopes feature a bit.
-3.0.0: Using Google Sign-In for iOS, instead of Google+.
+## 7. Changelog
 1.1.0: Added `isAvailable`, for issue [#37](https://github.com/EddyVerbruggen/cordova-plugin-googleplus/issues/37)
 1.0.0: Initial version supporting iOS and Android
 
-## 9. License
+## 8. License
 
 [The MIT License (MIT)](http://www.opensource.org/licenses/mit-license.html)
 
--- src/android/GooglePlus.java
+++ src/android/GooglePlus.java
@@ -1,12 +1,10 @@
 package nl.xservices.plugins;
 
-import android.Manifest;
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentSender;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import com.google.android.gms.auth.GoogleAuthException;
 import com.google.android.gms.auth.GoogleAuthUtil;
@@ -18,7 +16,6 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Scope;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.plus.Plus;
 import com.google.android.gms.plus.model.people.Person;
@@ -27,9 +24,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.List;
 
 public class GooglePlus extends CordovaPlugin implements ConnectionCallbacks, OnConnectionFailedListener {
 
@@ -40,53 +34,30 @@ public class GooglePlus extends CordovaPlugin implements ConnectionCallbacks, On
   public static final String ACTION_DISCONNECT = "disconnect";
   public static final String ARGUMENT_ANDROID_KEY = "androidApiKey";
   public static final String ARGUMENT_WEB_KEY = "webApiKey";
-  public static final String ARGUMENT_SCOPES = "scopes";
-  public static final String ARGUMENT_OFFLINE_KEY = "offline";
-
-  private static final int REQUEST_GET_ACCOUNTS = 1;
-
-  /**
-   * Email for the google account that is being logged in
-   */
-  private String email;
-  /**
-   * JSON of useful information from the result of a successful connection to the google services api
-   */
-  private JSONObject result;
-  /**
-   * List of scopes that we'll request from the google services api
-   */
-  private List<Scope> scopes = new ArrayList<Scope>();
 
   // Wraps our service connection to Google Play services and provides access to the users sign in state and Google APIs
   private GoogleApiClient mGoogleApiClient;
-  private String apiKey, webKey, scopesString;
+  private String apiKey, webKey;
   private CallbackContext savedCallbackContext;
   private boolean trySilentLogin;
   private boolean loggingOut;
-  private boolean requestOfflineToken;
 
   @Override
   public void initialize(CordovaInterface cordova, CordovaWebView webView) {
     super.initialize(cordova, webView);
+    mGoogleApiClient = buildGoogleApiClient();
   }
 
   @Override
   public boolean execute(String action, CordovaArgs args, CallbackContext callbackContext) throws JSONException {
     this.savedCallbackContext = callbackContext;
 
-    if (args.optJSONObject(0) != null) {
+    if (args.optJSONObject(0) != null){
       JSONObject obj = args.getJSONObject(0);
+      System.out.println(obj);
       this.webKey = obj.optString(ARGUMENT_WEB_KEY, null);
       this.apiKey = obj.optString(ARGUMENT_ANDROID_KEY, null);
-      this.requestOfflineToken = obj.optBoolean(ARGUMENT_OFFLINE_KEY, false);
-      this.setupScopes(obj.optString(ARGUMENT_SCOPES, null));
-      // possible scope change, so force a rebuild of the client
-      this.mGoogleApiClient = null;
     }
-
-    //It's important that we build the GoogleApiClient after setting up scopes so we know which scopes to request when setting up the google services api client.
-    buildGoogleApiClient();
 
     if (ACTION_IS_AVAILABLE.equals(action)) {
       final boolean avail = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this.cordova.getActivity().getApplicationContext()) == 0;
@@ -106,7 +77,7 @@ public class GooglePlus extends CordovaPlugin implements ConnectionCallbacks, On
         mGoogleApiClient.disconnect();
         // needed in onActivityResult when the connect method below comes back
         loggingOut = true;
-        buildGoogleApiClient();
+        mGoogleApiClient = buildGoogleApiClient();
         mGoogleApiClient.connect();
       } catch (IllegalStateException ignore) {
       }
@@ -120,24 +91,6 @@ public class GooglePlus extends CordovaPlugin implements ConnectionCallbacks, On
     return true;
   }
 
-  /**
-   * Setup scopes that we will request from the google plus api.  Defaults to profile and https://www.googleapis.com/auth/plus.login.
-   * See https://developers.google.com/+/web/api/rest/oauth for more info about scopes.
-   * @param scopes Space delimited list of scopes
-   */
-  private void setupScopes(String scopes) {
-    if (scopes != null) {
-      this.scopesString = scopes;
-      for(String scope : scopes.split(" ")) {
-        this.scopes.add(new Scope(scope));
-      }
-    } else {
-      this.scopesString = Scopes.PLUS_LOGIN;
-      this.scopes.add(Plus.SCOPE_PLUS_LOGIN);
-      this.scopes.add(Plus.SCOPE_PLUS_PROFILE);
-    }
-  }
-
   private void disconnect() {
     try {
       Plus.AccountApi.revokeAccessAndDisconnect(mGoogleApiClient)
@@ -146,7 +99,7 @@ public class GooglePlus extends CordovaPlugin implements ConnectionCallbacks, On
             public void onResult(Status status) {
               // mGoogleApiClient is now disconnected and access has been revoked.
               // Don't care if it was disconnected already (status != success).
-              buildGoogleApiClient();
+              mGoogleApiClient = buildGoogleApiClient();
               savedCallbackContext.success("disconnected");
             }
           });
@@ -155,29 +108,17 @@ public class GooglePlus extends CordovaPlugin implements ConnectionCallbacks, On
     }
   }
 
-  /**
-   * Build the GoogleApiClient if it has not already been built.
-   *
-   * @return Our GoogleApiClient
-   */
-  private synchronized GoogleApiClient buildGoogleApiClient() {
-    if (this.mGoogleApiClient != null) {
-      return this.mGoogleApiClient;
-    }
-
-    GoogleApiClient.Builder builder = new GoogleApiClient.Builder(webView.getContext())
+  private GoogleApiClient buildGoogleApiClient() {
+    return new GoogleApiClient.Builder(webView.getContext())
         .addConnectionCallbacks(this)
         .addOnConnectionFailedListener(this)
-        .addApi(Plus.API, Plus.PlusOptions.builder().build());
-
-    for (Scope scope : this.scopes) {
-      builder.addScope(scope);
-    }
-
-    this.mGoogleApiClient = builder.build();
-    return this.mGoogleApiClient;
+        .addApi(Plus.API, Plus.PlusOptions.builder().build())
+        .addScope(Plus.SCOPE_PLUS_LOGIN)
+        .addScope(Plus.SCOPE_PLUS_PROFILE)
+        .build();
   }
 
+  @SuppressWarnings({ "unchecked", "rawtypes" })
   private void resolveToken(final String email, final JSONObject result) {
     final Context context = this.cordova.getActivity().getApplicationContext();
 
@@ -187,38 +128,29 @@ public class GooglePlus extends CordovaPlugin implements ConnectionCallbacks, On
         String token;
 
         try {
-          if (GooglePlus.this.webKey != null) {
+          if (GooglePlus.this.webKey != null){
             // Retrieve server side tokens
             scope = "audience:server:client_id:" + GooglePlus.this.webKey;
             token = GoogleAuthUtil.getToken(context, email, scope);
             result.put("idToken", token);
-          }
-
-          // if API key or offline flag is set, then also get the OAuth access token
-          if (GooglePlus.this.apiKey != null) {
+          } else if (GooglePlus.this.apiKey != null) {
             // Retrieve the oauth token with offline mode
             scope = "oauth2:server:client_id:" + GooglePlus.this.apiKey;
-            scope += ":api_scope:" + GooglePlus.this.scopesString;
+            scope += ":api_scope:" + Scopes.PLUS_LOGIN;
             token = GoogleAuthUtil.getToken(context, email, scope);
-            // Since this is a short-lived one time token immediately remove it from
-            // the cache. This ensures a new token each time the user authenticates.
-            GoogleAuthUtil.clearToken(context, token);
             result.put("oauthToken", token);
-          } else if (GooglePlus.this.requestOfflineToken) {
+          } else {
             // Retrieve the oauth token with offline mode
             scope = "oauth2:" + Scopes.PLUS_LOGIN;
             token = GoogleAuthUtil.getToken(context, email, scope);
-            // Since this is a short-lived one time token immediately remove it from
-            // the cache. This ensures a new token each time the user authenticates.
-            GoogleAuthUtil.clearToken(context, token);
             result.put("oauthToken", token);
           }
         }
         catch (UserRecoverableAuthException userAuthEx) {
           // Start the user recoverable action using the intent returned by
           // getIntent()
-          cordova.setActivityResultCallback(GooglePlus.this);
-          cordova.getActivity().startActivityForResult(userAuthEx.getIntent(), /*requestCode*/0);
+          cordova.getActivity().startActivityForResult(userAuthEx.getIntent(),
+              Activity.RESULT_OK);
           return;
         }
         catch (IOException e) {
@@ -237,23 +169,6 @@ public class GooglePlus extends CordovaPlugin implements ConnectionCallbacks, On
     });
   }
 
-  private Bundle cacheBundle;
-
-  public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults) throws JSONException {
-    if (requestCode != REQUEST_GET_ACCOUNTS) {
-      return;
-    }
-
-    for (int i = 0; i < permissions.length; i++) {
-      if (permissions[i].equals(Manifest.permission.GET_ACCOUNTS) && grantResults[i] == PackageManager.PERMISSION_GRANTED) {
-        onConnected(cacheBundle);
-        return;
-      }
-    }
-
-    savedCallbackContext.error("Must allow Get Account permissions.");
-  }
-
   /**
    * onConnected is called when our Activity successfully connects to Google
    * Play services.  onConnected indicates that an account was selected on the
@@ -263,26 +178,10 @@ public class GooglePlus extends CordovaPlugin implements ConnectionCallbacks, On
    */
   @Override
   public void onConnected(Bundle connectionHint) {
+    final String email = Plus.AccountApi.getAccountName(mGoogleApiClient);
     final Person user = Plus.PeopleApi.getCurrentPerson(mGoogleApiClient);
 
-    // The new cordova permission API's are available since Cordova-Android 5,
-    // so if you're using API level 23 and want to deploy to Android 6,
-    // make sure you're building with Cordova-Android 5 or higher.
-    // .. it was either this or adding an Android Support library.
-    try {
-      Method hasPermissionMethod = cordova.getClass().getMethod("hasPermission", String.class);
-      if (hasPermissionMethod != null) {
-        if (!(Boolean) hasPermissionMethod.invoke(cordova, Manifest.permission.GET_ACCOUNTS)) {
-          Method requestPermissionMethod = cordova.getClass().getMethod("requestPermission", CordovaPlugin.class, int.class, String.class);
-          requestPermissionMethod.invoke(cordova, this, REQUEST_GET_ACCOUNTS, Manifest.permission.GET_ACCOUNTS);
-        }
-      }
-    } catch (Exception ignore) {
-    }
-
-    this.email = Plus.AccountApi.getAccountName(mGoogleApiClient);
-    this.result = new JSONObject();
-
+    final JSONObject result = new JSONObject();
     try {
       result.put("email", email);
       // in case there was no internet connection, this may be null
@@ -359,37 +258,11 @@ public class GooglePlus extends CordovaPlugin implements ConnectionCallbacks, On
     }
   }
 
-  /**
-   * Handle the user's action from the permissioning workflow.  In this workflow a user may be asked for access to his profile, email, offline access etc.
-   *
-   * There are three situations we cover:
-   *
-   * 1. The user clicks sign in. We are not yet connected to the google services api. (common case)
-   *
-   * 2. The user clicks sign in and we are already connected to the google services api.  This case is less common, and it's a bit confusing
-   * why we'd already be connected to the google services api before the user clicks sign in.  This case happens when the user sees multiple pages
-   * in the permission workflow.  The user will see multiple pages in the workflow when we connect to the google services api (successfully), but on
-   * connection google tells us that we should prompt the user for even more permissions that the first page of the workflow didn't ask for via
-   * a UserRecoverableAuthException.  For example, a user may first be prompted for basic profile access.  Then when we connect to the google services api
-   * it may suggest that we also need to prompt the user for offline access.  In this case we don't bother reconnecting to the google services api again.  Instead,
-   * we skip straight to resolving to the token.
-   *
-   * 3. The user clicks cancel.
-   *
-   * @param requestCode The request code originally supplied to startActivityForResult(),
-   * @param resultCode The integer result code returned by the child activity through its setResult().
-   * @param intent Information returned by the child activity
-   */
   @Override
   public void onActivityResult(int requestCode, final int resultCode, final Intent intent) {
     super.onActivityResult(requestCode, resultCode, intent);
-    if (mGoogleApiClient == null) {
-      buildGoogleApiClient();
-    }
-    if (!mGoogleApiClient.isConnected() && resultCode == Activity.RESULT_OK) {
+    if (resultCode == Activity.RESULT_OK) {
       mGoogleApiClient.connect();
-    } else if (resultCode == Activity.RESULT_OK) {
-      this.resolveToken(email, result);
     } else {
       this.savedCallbackContext.error("user cancelled");
     }
